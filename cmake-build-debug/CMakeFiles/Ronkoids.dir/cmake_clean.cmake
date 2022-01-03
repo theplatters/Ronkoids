1146e4b057de58152fc87616832d83310b9dac2a
theplatters <fscharnreitner@gmail.com>
2022-01-03 15:14:03 +0100

    ...

---
+++ cmake-build-debug/CMakeFiles/Ronkoids.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ronkoids.dir/src/Application.cpp.obj"
+  "CMakeFiles/Ronkoids.dir/src/Command.cpp.obj"
+  "CMakeFiles/Ronkoids.dir/src/CommandQueue.cpp.obj"
+  "CMakeFiles/Ronkoids.dir/src/States/GameState.cpp.obj"
+  "CMakeFiles/Ronkoids.dir/src/States/State.cpp.obj"
+  "CMakeFiles/Ronkoids.dir/src/States/StateStack.cpp.obj"
+  "CMakeFiles/Ronkoids.dir/src/main.cpp.obj"
+  "CMakeFiles/Ronkoids.dir/src/pch.cpp.obj"
+  "Ronkoids.exe"
+  "Ronkoids.exe.manifest"
+  "Ronkoids.pdb"
+  "libRonkoids.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ronkoids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
